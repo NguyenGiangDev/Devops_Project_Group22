9e18cb7316381a4f3d24c43f955b4f6447f29123
NguyenGiangDev <nguyengiangdevw@gmail.com>
2025-04-09 15:24:20 +0000

    demo

--- build_all.sh
+++ build_all.sh
@@ -2,7 +2,7 @@
 
 # Tắt BuildKit để tránh lỗi buildx
 export DOCKER_BUILDKIT=0  
-echo "🔧 BuildKit đã bị tắt để đảm bảo quá trình build chạy ổn định."
+echo "🔧 BuildKit đã bị tắt để đảm bảo quá trình build chạy ổn định. À thế à"
 
 # Docker Hub username
 DOCKER_USER="nguyengianguit"
